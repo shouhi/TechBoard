d85c54e6d053382eac20867423d2f2760da9ce59
ide Shouhi <shouhi0922@icloud.com>
2021-09-19 00:50:20 +0900

    prod環境deploy

--- Front/.nuxt/components/index.js
+++ Front/.nuxt/components/index.js
@@ -1,13 +1,4 @@
 export { default as BoardsList } from '../../components/dashboard/BoardsList.vue'
-export { default as CreateBoardDialog } from '../../components/dialog/CreateBoardDialog.vue'
-export { default as CreateNameDialog } from '../../components/dialog/CreateNameDialog.vue'
-export { default as EditBoardDialog } from '../../components/dialog/EditBoardDialog.vue'
-export { default as EditSharedCardDialog } from '../../components/dialog/EditSharedCardDialog.vue'
-export { default as EmailListDialog } from '../../components/dialog/EmailListDialog.vue'
-export { default as InstantLoginDialog } from '../../components/dialog/InstantLoginDialog.vue'
-export { default as InviteUser } from '../../components/dialog/InviteUser.vue'
-export { default as MembersDialog } from '../../components/dialog/MembersDialog.vue'
-export { default as SharedCardSizeDialog } from '../../components/dialog/SharedCardSizeDialog.vue'
 export { default as ColorSelect } from '../../components/common/ColorSelect.vue'
 export { default as ConfirmationDialog } from '../../components/common/ConfirmationDialog.vue'
 export { default as ContextMenu } from '../../components/common/ContextMenu.vue'
@@ -17,6 +8,15 @@ export { default as SnackBar } from '../../components/common/SnackBar.vue'
 export { default as FontColorSelect } from '../../components/common/fontColorSelect.vue'
 export { default as BoardAppBar } from '../../components/boards/BoardAppBar.vue'
 export { default as BoardMenuBar } from '../../components/boards/BoardMenuBar.vue'
+export { default as CreateBoardDialog } from '../../components/dialog/CreateBoardDialog.vue'
+export { default as CreateNameDialog } from '../../components/dialog/CreateNameDialog.vue'
+export { default as EditBoardDialog } from '../../components/dialog/EditBoardDialog.vue'
+export { default as EditSharedCardDialog } from '../../components/dialog/EditSharedCardDialog.vue'
+export { default as EmailListDialog } from '../../components/dialog/EmailListDialog.vue'
+export { default as InstantLoginDialog } from '../../components/dialog/InstantLoginDialog.vue'
+export { default as InviteUser } from '../../components/dialog/InviteUser.vue'
+export { default as MembersDialog } from '../../components/dialog/MembersDialog.vue'
+export { default as SharedCardSizeDialog } from '../../components/dialog/SharedCardSizeDialog.vue'
 export { default as AppBar } from '../../components/layouts/AppBar.vue'
 export { default as AppealCard } from '../../components/top/AppealCard.vue'
 export { default as PricingCard } from '../../components/top/PricingCard.vue'
@@ -33,15 +33,6 @@ export { default as StockMenuBar } from '../../components/boards/stock-view/Stoc
 export { default as StockView } from '../../components/boards/stock-view/StockView.vue'
 
 export const LazyBoardsList = import('../../components/dashboard/BoardsList.vue' /* webpackChunkName: "components/boards-list" */).then(c => c.default || c)
-export const LazyCreateBoardDialog = import('../../components/dialog/CreateBoardDialog.vue' /* webpackChunkName: "components/create-board-dialog" */).then(c => c.default || c)
-export const LazyCreateNameDialog = import('../../components/dialog/CreateNameDialog.vue' /* webpackChunkName: "components/create-name-dialog" */).then(c => c.default || c)
-export const LazyEditBoardDialog = import('../../components/dialog/EditBoardDialog.vue' /* webpackChunkName: "components/edit-board-dialog" */).then(c => c.default || c)
-export const LazyEditSharedCardDialog = import('../../components/dialog/EditSharedCardDialog.vue' /* webpackChunkName: "components/edit-shared-card-dialog" */).then(c => c.default || c)
-export const LazyEmailListDialog = import('../../components/dialog/EmailListDialog.vue' /* webpackChunkName: "components/email-list-dialog" */).then(c => c.default || c)
-export const LazyInstantLoginDialog = import('../../components/dialog/InstantLoginDialog.vue' /* webpackChunkName: "components/instant-login-dialog" */).then(c => c.default || c)
-export const LazyInviteUser = import('../../components/dialog/InviteUser.vue' /* webpackChunkName: "components/invite-user" */).then(c => c.default || c)
-export const LazyMembersDialog = import('../../components/dialog/MembersDialog.vue' /* webpackChunkName: "components/members-dialog" */).then(c => c.default || c)
-export const LazySharedCardSizeDialog = import('../../components/dialog/SharedCardSizeDialog.vue' /* webpackChunkName: "components/shared-card-size-dialog" */).then(c => c.default || c)
 export const LazyColorSelect = import('../../components/common/ColorSelect.vue' /* webpackChunkName: "components/color-select" */).then(c => c.default || c)
 export const LazyConfirmationDialog = import('../../components/common/ConfirmationDialog.vue' /* webpackChunkName: "components/confirmation-dialog" */).then(c => c.default || c)
 export const LazyContextMenu = import('../../components/common/ContextMenu.vue' /* webpackChunkName: "components/context-menu" */).then(c => c.default || c)
@@ -51,6 +42,15 @@ export const LazySnackBar = import('../../components/common/SnackBar.vue' /* web
 export const LazyFontColorSelect = import('../../components/common/fontColorSelect.vue' /* webpackChunkName: "components/font-color-select" */).then(c => c.default || c)
 export const LazyBoardAppBar = import('../../components/boards/BoardAppBar.vue' /* webpackChunkName: "components/board-app-bar" */).then(c => c.default || c)
 export const LazyBoardMenuBar = import('../../components/boards/BoardMenuBar.vue' /* webpackChunkName: "components/board-menu-bar" */).then(c => c.default || c)
+export const LazyCreateBoardDialog = import('../../components/dialog/CreateBoardDialog.vue' /* webpackChunkName: "components/create-board-dialog" */).then(c => c.default || c)
+export const LazyCreateNameDialog = import('../../components/dialog/CreateNameDialog.vue' /* webpackChunkName: "components/create-name-dialog" */).then(c => c.default || c)
+export const LazyEditBoardDialog = import('../../components/dialog/EditBoardDialog.vue' /* webpackChunkName: "components/edit-board-dialog" */).then(c => c.default || c)
+export const LazyEditSharedCardDialog = import('../../components/dialog/EditSharedCardDialog.vue' /* webpackChunkName: "components/edit-shared-card-dialog" */).then(c => c.default || c)
+export const LazyEmailListDialog = import('../../components/dialog/EmailListDialog.vue' /* webpackChunkName: "components/email-list-dialog" */).then(c => c.default || c)
+export const LazyInstantLoginDialog = import('../../components/dialog/InstantLoginDialog.vue' /* webpackChunkName: "components/instant-login-dialog" */).then(c => c.default || c)
+export const LazyInviteUser = import('../../components/dialog/InviteUser.vue' /* webpackChunkName: "components/invite-user" */).then(c => c.default || c)
+export const LazyMembersDialog = import('../../components/dialog/MembersDialog.vue' /* webpackChunkName: "components/members-dialog" */).then(c => c.default || c)
+export const LazySharedCardSizeDialog = import('../../components/dialog/SharedCardSizeDialog.vue' /* webpackChunkName: "components/shared-card-size-dialog" */).then(c => c.default || c)
 export const LazyAppBar = import('../../components/layouts/AppBar.vue' /* webpackChunkName: "components/app-bar" */).then(c => c.default || c)
 export const LazyAppealCard = import('../../components/top/AppealCard.vue' /* webpackChunkName: "components/appeal-card" */).then(c => c.default || c)
 export const LazyPricingCard = import('../../components/top/PricingCard.vue' /* webpackChunkName: "components/pricing-card" */).then(c => c.default || c)
